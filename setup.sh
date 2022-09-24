df403054ef9e94bea02f8d6afdfab073664d6561
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-09-24 16:20:13 +0530

    setup: determine current dir with command

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+CURRENT_DIR="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)";
+
 # Packages
-udo apt install zsh curl
+sudo apt install zsh curl
 
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
@@ -10,7 +14,8 @@ curl -L git.io/antigen > ~/antigen.zsh
 # Starship
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 mkdir -p ~/.config
-mv starship.toml ~/.config/starship.toml
+mv $CURRENT_DIR/starship.toml ~/.config/starship.toml
+mv $CURRENT_DIR/.zshrc ~/.zshrc
 
 # Start ZSH
 exec zsh
